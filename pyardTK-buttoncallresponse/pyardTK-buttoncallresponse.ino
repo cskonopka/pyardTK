6a1f33d3a021125a62edd86c22ee79aa1bcd2c32
Christopher Konopka <cskonopka@gmail.com>
2016-05-17 17:16:04 -0400

    working example for pi and desktop with an arduino

--- pyardTK-buttoncallresponse/pyardTK-buttoncallresponse.ino
+++ pyardTK-buttoncallresponse/pyardTK-buttoncallresponse.ino
@@ -1,77 +1,33 @@
-//
-//String inputString = "";         // a string to hold incoming data
-//boolean stringComplete = false; 
-//
-//void setup() 
-//{ 
-//  //  Create/open serial port
-//    Serial.begin(9600);   
-//      inputString.reserve(200);
-//}
-//
-//void loop() 
-//{
-//int j =1;
-// while (j==1){
-//  Serial.write(12);
-// }
-////   while (Serial.available()) {
-////    // get the new byte:
-////    char inChar = (char)Serial.read(); 
-////    // add it to the inputString:
-////    inputString += inChar;
-////    Serial.println(inChar);
-////    Serial.println(inputString);
-////    // if the incoming character is a newline, set a flag
-////    // so the main loop can do something about it:
-////    if (inChar == '\n') {
-////      stringComplete = true;
-////    } 
-////          Serial.println(10);
-////  }
-////    if (stringComplete) {
-////    Serial.println(inputString); 
-//////    Serial.write('hello');
-////    // clear the string:
-////    inputString = "";
-////    stringComplete = false;
-//////          Serial.println(10);
-////  }
-////        Serial.write(10);
-//}
-
-  int cupcake = 1;
 void setup(){
+  // Setup serial port 
   Serial.begin(9600);
-
 }
 
 void loop(){
 
-//while (cupcake == 1){
+  // Tap and receive a single stream from the serial port
   int8_t inner = Serial.parseInt();
 
-
+  // Receive the data from the button press
+  // The flush orientation is what enabled singular button presses to
+  // work otherwise you need to double click and a value gets stuck
+  // in the buffer. This has proven to be the most efficient setup for 
+  // the current design parameters. The delays are needed to create 
+  // enough space for data to travel and not clog the pipe. If you 
+  // wanted a faster reaction time the delay messages is what you would 
+  // play around with but beware, it can clog up fast.
   if (inner == 1){
     Serial.print('1',DEC);
     Serial.flush();
-//    Serial.println(' ');
     delay(0.1);
     Serial.flush();    
-
   }
   if (inner == 2){
     Serial.print('2',DEC);
     Serial.flush();
-//    Serial.println(' ');
     delay(0.1);
     Serial.flush();
-
   }
   Serial.flush();
   delay(1);
-
-  
-//}
-  
 }
